717b30804698cb5d30acc17982ca129be3a4c0f5
banan88 <mat.banaszkiewicz@gmail.com>
2011-04-18 22:31:10 +0200

    breslau weekend:

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-# Create your views here.
 # -*- coding: utf-8 -*-
+##TO DO: render_to_response('template.html', locals())!!!
 
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.template import Context, loader
 from main.models import Vet, Client, Animal
 from wet import settings
@@ -15,43 +16,34 @@ def getDate():
     return txt
 
 def index(request):
-    template = loader.get_template('main/index.html')
     title = "Strona główna"
     context = Context({'title':title, 'STATIC_URL':settings.STATIC_URL, 'date':getDate()})
-    return HttpResponse(template.render(context))
+    return render_to_response('main/index.html',context)
 
 def vets(request):
     vetlist = Vet.objects.all().order_by('-pk')
-    #all_clients = []
-    #for vet in vetlist:
-        #if not vet.client_set.all().isEmpty():
-            
-    template = loader.get_template('main/weterynarze.html')
     context = Context({'vetlist' : vetlist,})
-    return HttpResponse(template.render(context))
+    return render_to_response('main/weterynarze.html',context)
 
 def vet_details(request, vet_id):
     vet = Vet.objects.get(pk=vet_id)
     clientlist = vet.client_set.all()
-    template = loader.get_template('main/weterynarze_details.html')
     context = Context({'clientlist': clientlist, 'vet':vet, 'len':len(clientlist)})
-    return HttpResponse(template.render(context))
+    return render_to_response('main/weterynarze_details.html',context)
 
 def client_details(request, client_id):
     client = Client.objects.get(pk=client_id)
     vet = client.vet
     animallist = client.animal_set.all()
-    template = loader.get_template('main/klient_details.html')
     context = Context({'animallist': animallist, 'client':client, 'vet':vet})
-    return HttpResponse(template.render(context))
+    return render_to_response('main/klient_details.html', context)
 
 def animal_details(request, animal_id):
     animal = Animal.objects.get(pk=animal_id)
     client = animal.client
     vet = client.vet
     age = animal.age()
-    template = loader.get_template('main/zwierzak_details.html')
     context = Context({'wiek':age, 'animal':animal, 'client':client, 'vet':vet})
-    return HttpResponse(template.render(context));
+    return render_to_response('main/zwierzak_details.html', context);
     
     
